d5b275f73cc7de08cb54d54ea6400492463771d3
JL <19521733@gm.uit.edu.vn>
2021-11-04 23:18:17 +0700

    addHomePage-UserBlogPage-Navbar/formatFolder

---
+++ js/home.js
@@ -0,0 +1,48 @@
+
+const slides = document.querySelectorAll('.slides'),
+    dot = document.querySelectorAll('.dot');
+
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoslide, 8000);
+function autoslide() {
+    counter =+ 1;
+    slidefun(counter);
+}
+
+function plusSlides(n) {
+    counter += n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function currentSlide(n) {
+    counter = n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    timer = setInterval(autoslide, 8000);
+}
+
+function slidefun(n) {
+    let i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dot.length; i++) {
+        dot[i].classList.remove('active');
+    }
+    if (n > slides.length) {
+        counter = 1;
+    }
+    if (n < 1) {
+        counter = slides.length;
+    }
+    slides[counter - 1].style.display = "block";
+    dot[counter - 1].classList.add('active');
+}
+
